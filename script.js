a0742d2750a44d41fca1191d255c37d883149f83
Swadhin <swadhinghosh31@gmail.com>
2023-02-11 15:01:52 +0530

    initial commit

--- script.js
+++ script.js
@@ -8,20 +8,39 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.filter((arr)=> arr.marks > 50)
+  arr.map((arr)=>console.log(arr));
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((arr) => {
+    if(arr.marks > 50){
+      console.log(arr);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newarr = {id: 4, name: "susan", age: "20", marks: 45};
+  arr.push(newarr);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let updatedarr  = arr.filter((arr) => arr.marks <= 50);
+  console.log(updatedarr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newarr = [
+    { id: 5, name: "tim", age: "22", marks: 60 },
+    { id: 6, name: "jane", age: "18", marks: 75 },
+    { id: 7, name: "sam", age: "20", marks: 80 },
+  ];
+  let concatenateArra = arr.concat(newarr);
+  console.log(concatenateArra);
 }
